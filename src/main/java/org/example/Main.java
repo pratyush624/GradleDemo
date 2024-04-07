e7405e4c0f4395b22aaeedabfa07e9f1149555c5
Pratyush <pratyush624@gmail.com>
2024-04-07 11:40:14 +0530

    Added retrofit support with GSON

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,16 +3,18 @@ package org.example;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 import java.io.IOException;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         // This library helps us to do http request easily
-        OkHttpClient client = new OkHttpClient();
+/*        OkHttpClient client = new OkHttpClient();
 
         String url = "https://jsonplaceholder.typicode.com/todos/1";
 
@@ -25,6 +27,15 @@ public class Main {
             System.out.println("Data : " + response.body().string());
         }catch (IOException ex){
             ex.printStackTrace();
-        }
+        }*/
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        TodoService todoService = retrofit.create(TodoService.class);
+        Todo t = todoService.getTodoById("1").execute().body();
+        System.out.println("Todo object downloaded is : " + t.toString());
     }
 }
